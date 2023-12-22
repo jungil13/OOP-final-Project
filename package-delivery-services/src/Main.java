a90736c6b884fc7793bc248f51c2dc0b466a5b38
jungil13 <145642451+jungil13@users.noreply.github.com>
2023-12-22 11:11:49 +0800

    Add files via upload

---
+++ package-delivery-services/src/Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        UserInterface ui = new UserInterface();
+        System.out.println("Hello World!");
+    }
+}
